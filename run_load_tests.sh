d4bd5e37ccd4af9314c4f30e39e0124fd40f0b55
rutkin <r.utkin91@gmail.com>
2025-06-06 23:39:56 +0300

    dialog perf test

--- run_load_tests.sh
+++ run_load_tests.sh
@@ -7,8 +7,8 @@ rm -rf ./load-tests/results/*
 # Run load tests and wait for completion
 echo "Starting load tests..."
 # docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit
-docker-compose -f docker-compose.yml -f docker-compose.jmeter.yml up --build --abort-on-container-exit
+docker-compose -f docker-compose.yml -f docker-compose.jmeter.yml up --build #--abort-on-container-exit
 
 # Stop containers after tests
-echo "Stopping containers..."
-docker-compose -f docker-compose.yml -f docker-compose.jmeter.yml down 
+# echo "Stopping containers..."
+# docker-compose -f docker-compose.yml -f docker-compose.jmeter.yml down 
